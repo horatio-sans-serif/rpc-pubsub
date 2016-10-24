3d811d32a4c8a722b536bd274124d14c1cc10b3b
REDACTED <REDACTED_EMAIL>
2016-10-24 11:45:27 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,46 @@
+A set of handlers for rpc-over-ws for PUBSUB using Redis' PUBSUB to scale out.
+
+## Installation
+
+    npm i --save rpc-pubsub
+
+## Usage
+
+    const Redis = require('ioredis')
+    const redis = new Redis(process.env.REDIS_URL)
+    const {publish, subscribe, unsubscribe, disconnected}
+
+    const {server} = require('rpc-over-ws')({
+      publish,
+      subscribe,
+      unsubscribe
+    })
+
+    server.on('client-disconnect', client => disconnected(client))
+
+## Handlers
+
+Note that "this" (the remote client) must be authenticated already with
+`.clientId` and `.alias` set before these handlers will work.
+
+      subscribe({channel})       -> Promise<void>
+      publish({channel, data})   -> Promise<void>
+      unsubscribe({channel})     -> Promise<void>
+
+## Notifications
+
+Clients are sent the following JSON when a message is published to a channel
+on which they are subscribed.
+
+    {
+      "type": "published",
+      "timestamp": int,
+      "channel": string,
+      "alias": string,
+      "data": any
+    }
+
+## Notes
+
+There is no store-and-forward. A client must be connected and subscribed
+to be sent messages published to a channel.
---
+++ index.js
@@ -0,0 +1,86 @@
+const _ = require('lodash')
+const shortid = require('shortid')
+const EventEmitter = require('events')
+
+module.exports = function configurePUBSUB (redis, options = {}) {
+  const localServerId = options.serverId || shortid()
+  const localSubs = {}
+
+  const emitter = new EventEmitter()
+
+  function sendToLocalSubs(channel, alias, data) {
+    const timestamp = _.now() / 1000 | 0
+    const toSend = JSON.stringify({ type: 'published', timestamp, channel, alias, data })
+    _.each(localSubs[channel] || {}, socket => socket.send(toSend))
+  }
+
+  function sendToRemoteSubs(channel, alias, data) {
+    redis.publish(channel, JSON.stringify({
+      serverId: localServerId, channel, alias, data
+    }))
+  }
+
+  const subRedis = redis.duplicate()
+
+  subRedis.on('message', (channel, message) => {
+    try {
+      message = JSON.parse(message)
+      if (message.serverId !== localServerId)
+        sendToLocalSubs(channel, message.alias, message.data)
+    } catch (err) {
+      console.error('redis error', err)
+    }
+  })
+
+  function subscribe({channel}) {
+    return new Promise((resolve, reject) => {
+      if (!this.clientId || !this.alias)
+        return reject('authentication required')
+
+      subRedis.subscribe(channel)
+      _.set(localSubs, `${channel}.${this.clientId}`, socket)
+      _.set(this, `channels.${channel}`, true)
+
+      resolve()
+    })
+  }
+
+  function unsubscribe({channel}) {
+    return new Promise((resolve, reject) => {
+      if (!this.clientId || !this.alias)
+        return reject('authentication required')
+
+      _.unset(socket.channels, channel)
+      _.unset(localSubs, `${channel}.${this.clientId}`)
+
+      if (_.isEmpty(localSubs[channel])) {
+        _.unset(localSubs, channel)
+        subRedis.unsubscribe(channel)
+      }
+
+      resolve()
+    })
+  }
+
+  function publish({channel, data}) {
+    return new Promise((resolve, reject) => {
+      if (!this.clientId || !this.alias)
+        return reject('authentication required')
+
+      sendToLocalSubs(channel, this.clientId, data)
+      sendToRemoteSubs(channel, this.clientId, data)
+
+      resolve()
+    })
+  }
+
+  return {
+    subscribe,
+    unsubscribe,
+    publish,
+    disconnected: (client) => {
+      _.each(client.channels, channel => unsubscribe.call(client, {channel}))
+    },
+    emitter
+  }
+}
